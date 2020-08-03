85ec7a14ac1695935b9bf36dcd6d24cfa97c3139
jiangxiaowu <jiangxiaowu6868@gmail.com>
2020-08-03 23:29:35 +0800

    first project

---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <h1>Videos</h1>
+    <div class="video-container">
+      <div v-for="video in videos" :key="video.name">
+        <router-link :to="{ name: 'video-watch' , params: { id:video.id }}">
+          <div class="video-box">
+            <img :src="video.thumbnail" />
+            <div>
+              <h3>{{ video.name }}</h3>
+              <div v-html="video.description"></div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      videos: this.$store.state.videos
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.video-container {
+  .video-box {
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+
+    text-align: left;
+
+    display: flex;
+    justify-content: flex-start;
+
+    img {
+      width: 250px;
+      padding: 10px;
+    }
+  }
+}
+</style>
